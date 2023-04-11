094d65e206c814cd24cdf0d648f11ea6f52b455b
KaDw <karol1dw@gmail.com>
2023-04-11 20:31:29 +0200

    handle errors in decrypt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, ValueEnum};
+use std::fmt::Display;
 use std::io;
 use std::path::Path;
 use std::str;
@@ -36,45 +37,74 @@ enum Mode {
     Decrypt,
 }
 
-fn decrypt(fname: &str, password: &str, xor: u8) {
+#[derive(Debug)]
+enum DecryptEncryptError {
+    Io(io::Error),
+    Zip(zip::result::ZipError),
+    ZipInvalidPassword(zip::result::InvalidPassword),
+}
+
+impl Display for DecryptEncryptError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+impl From<io::Error> for DecryptEncryptError {
+    fn from(error: io::Error) -> Self {
+        DecryptEncryptError::Io(error)
+    }
+}
+
+impl From<zip::result::ZipError> for DecryptEncryptError {
+    fn from(error: zip::result::ZipError) -> Self {
+        DecryptEncryptError::Zip(error)
+    }
+}
+
+impl From<zip::result::InvalidPassword> for DecryptEncryptError {
+    fn from(error: zip::result::InvalidPassword) -> Self {
+        DecryptEncryptError::ZipInvalidPassword(error)
+    }
+}
+
+fn decrypt(fname: &str, password: &str, xor: u8) -> Result<(), DecryptEncryptError> {
     let extract_path = Path::new("config_decrypted");
     fs::create_dir_all(extract_path).expect("Failed to create");
-    let file = File::open(Path::new(fname)).unwrap();
+    let file = File::open(Path::new(fname))?;
 
-    let mut archive = zip::ZipArchive::new(file).unwrap();
-    println!("Found {} files", archive.len());
+    let mut archive = zip::ZipArchive::new(file)?;
+    let dat_files = archive.file_names().filter(|x| x.ends_with(".dat")).count();
+    println!("Found {} .dat files", dat_files);
     for i in 0..archive.len() {
-        let mut ufile = archive
-            .by_index_decrypt(i, password.as_bytes())
-            .unwrap()
-            .unwrap();
-
+        let mut ufile = archive.by_index_decrypt(i, password.as_bytes())??;
         let outpath = match ufile.enclosed_name() {
             Some(p) => extract_path.join(p.to_owned()),
             None => continue,
         };
         if (*ufile.name()).ends_with('/') {
-            fs::create_dir_all(&outpath).unwrap();
+            fs::create_dir_all(&outpath)?;
         } else {
             if let Some(p) = outpath.parent() {
                 if !p.exists() {
-                    fs::create_dir_all(p).unwrap();
+                    fs::create_dir_all(p)?;
                 }
             }
-            let mut outfile = File::create(&outpath).unwrap();
+            let mut outfile = File::create(&outpath)?;
             if (*ufile.name()).ends_with(".dat") {
                 let mut vec: Vec<u8> = Vec::with_capacity(ufile.size() as usize);
-                ufile.read_to_end(&mut vec).unwrap();
+                ufile.read_to_end(&mut vec)?;
                 vec = vec
                     .iter_mut()
                     .map(|d| DECODE_TABLE[256_usize * xor as usize + *d as usize])
                     .collect();
-                outfile.write(&vec).unwrap();
+                outfile.write(&vec)?;
             } else {
-                io::copy(&mut ufile, &mut outfile).unwrap();
+                io::copy(&mut ufile, &mut outfile)?;
             }
         }
     }
+    Ok(())
 }
 
 fn main() {
@@ -86,7 +116,10 @@ fn main() {
         }
         Mode::Decrypt => {
             println!("Extracting and decrypting {}...", &cli.file);
-            decrypt(&cli.file, &cli.password, cli.xor);
+            match decrypt(&cli.file, &cli.password, cli.xor) {
+                Ok(_) => {}
+                Err(e) => println!("{:?}", e),
+            }
         }
     }
 }
